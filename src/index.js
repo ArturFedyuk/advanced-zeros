38d85a070f609a3c4ad1788294322e6c8d936510
Artur <artur-fedyuk@mail.ru>
2018-03-02 18:24:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  count = num;
+  date = base;
+  a, b, c = 0;
+
+  for (i = 2; i <= base; i++) 
+  {
+      a = 0;
+      c = num;
+
+      if (date % i == 0) 
+      {
+      b = 0;
+      while (date % i == 0) 
+      {
+        b++;
+        date = Math.floor(date / i);
+      }
+
+      while (c / i > 0)
+      {
+        a += Math.floor(c / i);
+        c = Math.floor(c / i);
+      }
+      count = Math.floor(Math.min(count, a / b));
+    }
+  }
+return count;
 }
